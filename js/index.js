eb33c7703116494122517e4ebf8d588a617d264a
DLee <DLee@DLee-PC>
2015-12-02 23:34:54 +0800

    css

--- js/index.js
+++ js/index.js
@@ -1,8 +1,42 @@
 $(function(){
+	var l = $(".menuItem").length;	//导航菜单数量
+	$(".menuList").width(l * 180 + "px");
 	//导航切换
 	$(".menuItem").click(function(){
 		var _s = $(this);
 		_s.addClass("active").siblings().removeClass("active");
+	});
+	//导航滚动
+	var i = 1;
+	var c = l - 4;	//可点击次数
+	$(".menuBtn.right").click(function(){
+		if(i == c){
+			$(this).css("display", "none");
+			$(".menuBtn.left").css("display", "block");
+		} else if(i > c){
+			$(this).css("display", "none");
+			$(".menuBtn.left").css("display", "block");
+			return;
+		}
+		$(".menuList").animate({
+			"marginLeft": - i * 180 + "px"
+		});
+		i++;
+	});
+	$(".menuBtn.left").click(function(){
+		if(i == 2){
+			$(this).css("display", "none");
+			$(".menuBtn.right").css("display", "block");
+		} else if(i < 2){
+			$(this).css("display", "none");
+			$(".menuBtn.right").css("display", "block");
+			return;
+		}
+		var ml = parseInt($(".menuList").css("margin-left"));
+		$(".menuList").animate({
+			"marginLeft": ml + 180 + "px"
+		});
+		i--;
 	});
 	//线上、线下课程
 	$("#rMenu>div").hover(function(){
