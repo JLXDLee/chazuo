dd6099e480f3620680084b0da5e3cca89cdfc74a
DLee <DLee@DLee-PC>
2015-12-03 22:27:06 +0800

    index

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+var cliend_url = "http://www.jlxdlee.com";
 $(function(){
 	var l = $(".menuItem").length;	//导航菜单数量
 	$(".menuList").width(l * 180 + "px");
@@ -52,4 +53,19 @@ $(function(){
 		$(this).hide();
 		$("#loaded1").show();
 	});
+	//客户端
+	createClientCode();
+	$("#client").mouseover(function(){
+		$("#clientCode").show();
+	}).mouseout(function(){
+		$("#clientCode").hide();
+	});
 });
+//生成客户端下载二维码
+function createClientCode(){
+	var qrcode = new QRCode(document.getElementById("ccCodeImg"), {
+		width: 90, //设置宽高
+		height: 90
+	});
+	qrcode.makeCode(cliend_url);
+}
