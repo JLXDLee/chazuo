6292edf8c21c20db0056413ad7594d7cf0bf758f
jlxdlee <447518349@qq.com>
2015-12-06 21:22:09 +0800

    q

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var cliend_url = "http://www.jlxdlee.site";	//配置客户端下载地址
+
 $(function(){
 	var l = $(".menuItem").length;	//导航菜单数量
 	$(".menuList").width(l * 180 + "px");
@@ -43,32 +43,9 @@ $(function(){
 		});
 		i--;
 	});
-	//线上、线下课程
-	$("#rMenu>div").hover(function(){
-		$(this).addClass("rmcActive").siblings().removeClass("rmcActive");
-	});
 	//加载更多
 	$("#loadMore").click(function(){
 		$(this).hide();
 		$("#loaded").show();
 	});
-	$("#loadMore1").click(function(){
-		$(this).hide();
-		$("#loaded1").show();
-	});
-	//客户端
-	createClientCode();
-	$("#client").mouseover(function(){
-		$("#clientCode").show();
-	}).mouseout(function(){
-		$("#clientCode").hide();
-	});
 });
-//生成客户端下载二维码
-function createClientCode(){
-	var qrcode = new QRCode(document.getElementById("ccCodeImg"), {
-		width: 90, //设置宽高
-		height: 90
-	});
-	qrcode.makeCode(cliend_url);
-}
